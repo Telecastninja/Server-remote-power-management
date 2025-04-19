bb65f31f3a7f738f593212f902f1a528f67ee731
Telecastninja <126715158+Telecastninja@users.noreply.github.com>
2025-04-19 06:03:54 +0200

    Delete Start_Server.bat

--- Start_Server.bat
+++
@@ -1,8 +0,0 @@
-REM Start_Server.bat
-REM Purpose: Sends a Wake-on-LAN packet to start server
-@echo off
-REM Fill out bracked parts with your information
-echo Sending Wake-on-LAN signal to <YOUR_SERVER_HOSTNAME> (<YOUR_SERVER_MAC>)...
-<CLIENT_CONTROL_FOLDER_PATH>\Files\WakeMeOnLan.exe /wakeup <YOUR_SERVER_MAC>
-echo Signal sent.
-pause
